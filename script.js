4dfb89f35b728507cb164f77bca50703a2cbbc67
ragecagebelt <jlscvngr@gmail.com>
2026-06-25 21:51:38 -0700

    Initial

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.getElementById('playButton');
+
+  if (button) {
+    button.addEventListener('click', () => {
+      alert('Play action to be implemented.');
+    });
+  }
+});
